86f04a47e328fc5c7998233b73a41a7a5b27a285
youchenlong <2091838306@qq.com>
2024-03-27 17:21:36 +0800

    add macc from commnet

---
+++ baselines/macc.py
@@ -0,0 +1,198 @@
+import torch
+import torch.nn.functional as F
+from torch import nn
+
+from models import MLP
+import sys 
+sys.path.append("..") 
+from action_utils import select_action, translate_action
+
+class MACC(nn.Module):
+    def __init__(self, args):
+        self.args = args
+        self.nagents = args.nagents
+        self.hid_size = args.hid_size
+        self.comm_passes = args.comm_passes
+        self.recurrent = args.recurrent
+
+        self.continuous = args.continuous
+        if self.continuous:
+            self.action_mean = nn.Linear(args.hid_size, args.dim_actions)
+            self.action_log_std = nn.Parameter(torch.zeros(1, args.dim_actions))
+        else:
+            self.action_heads = nn.ModuleList([nn.Linear(args.hid_size, o)
+                                        for o in args.naction_heads])
+        # self.init_std = args.init_std if hasattr(args, 'comm_init_std') else 0.2
+        
+        # Mask for communication
+        if self.args.comm_mask_zero:
+            self.comm_mask = torch.zeros(self.nagents, self.nagents)
+        else:
+            self.comm_mask = torch.ones(self.nagents, self.nagents) \
+                            - torch.eye(self.nagents, self.nagents)
+
+        self.encoder = nn.Linear(num_inputs, args.hid_size)
+
+        # if args.recurrent:
+        #     self.hidd_encoder = nn.Linear(args.hid_size, args.hid_size)
+
+        if args.recurrent:
+            self.init_hidden(args.batch_size)
+            self.f_module = nn.LSTMCell(args.hid_size, args.hid_size)
+        else:
+            if args.share_weights:
+                self.f_module = nn.Linear(args.hid_size, args.hid_size)
+                self.f_modules = nn.ModuleList([self.f_module
+                                                for _ in range(self.comm_passes)])
+            else:
+                self.f_modules = nn.ModuleList([nn.Linear(args.hid_size, args.hid_size)
+                                                for _ in range(self.comm_passes)])
+
+        if args.share_weights:
+            self.C_module = nn.Linear(args.hid_size, args.hid_size)
+            self.C_modules = nn.ModuleList([self.C_module
+                                            for _ in range(self.comm_passes)])
+        else:
+            self.C_modules = nn.ModuleList([nn.Linear(args.hid_size, args.hid_size)
+                                            for _ in range(self.comm_passes)])
+        
+        if args.comm_init == 'zeros':
+            for i in range(self.comm_passes):
+                self.C_modules[i].weight.data.zero_()
+
+        self.tanh = nn.Tanh()
+
+        self.value_head = nn.Linear(self.hid_size, 1)
+        pass
+
+    def get_agent_mask(self, batch_size, info):
+        n = self.nagents
+
+        if 'alive_mask' in info:
+            agent_mask = torch.from_numpy(info['alive_mask'])
+            num_agents_alive = agent_mask.sum()
+        else:
+            agent_mask = torch.ones(n)
+            num_agents_alive = n
+
+        agent_mask = agent_mask.view(1, 1, n)
+        agent_mask = agent_mask.expand(batch_size, n, n).unsqueeze(-1).clone() # clone gives the full tensor and avoid the error
+
+        return num_agents_alive, agent_mask
+
+    def forward_state_encoder(self, x):
+        hidden_state, cell_state = None, None
+
+        if self.args.recurrent:
+            x, extras = x
+            x = self.encoder(x)
+
+            if self.args.rnn_type == 'LSTM':
+                hidden_state, cell_state = extras
+            else:
+                hidden_state = extras
+            # hidden_state = self.tanh( self.hidd_encoder(prev_hidden_state) + x)
+        else:
+            x = self.encoder(x)
+            x = self.tanh(x)
+            hidden_state = x
+
+        return x, hidden_state, cell_state
+
+    def forward(self, x, info={}):
+        """
+        Forward function of MACC (two rounds of communication)
+
+        Arguments:
+            x (list): a list for the input of the communication protocol [observations, (previous hidden states, previous cell states)]
+            observations (tensor): the observations for all agents [1 (batch_size) * n * obs_size]
+            previous hidden/cell states (tensor): the hidden/cell states from the previous time steps [n * hid_size]
+
+        Returns:
+            action_out (list): a list of tensors of size [1 (batch_size) * n * num_actions] that represent output policy distributions
+            value_head (tensor): estimated values [n * 1]
+            next hidden/cell states (tensor): next hidden/cell states [n * hid_size]
+        """
+
+        x, hidden_state, cell_state = self.forward_state_encoder(x)
+
+        batch_size = x.size()[0]
+        n = self.nagents
+
+        num_agents_alive, agent_mask = self.get_agent_mask(batch_size, info)
+        agent_mask_transpose = agent_mask.transpose(1, 2)
+
+        for i in range(self.comm_passes):
+            # Choose current or prev depending on recurrent
+            comm = hidden_state.view(batch_size, n, self.hid_size) if self.args.recurrent else hidden_state
+
+            # Get the next communication vector based on next hidden state
+            comm = comm.unsqueeze(-2).expand(-1, n, n, self.hid_size)
+
+            # Create mask for masking self communication
+            mask = self.comm_mask.view(1, n, n)
+            mask = mask.expand(comm.shape[0], n, n)
+            mask = mask.unsqueeze(-1)
+            mask = mask.expand_as(comm)
+            
+            comm = comm * mask
+
+            if hasattr(self.args, 'comm_mode') and self.args.comm_mode == 'avg' \
+                and num_agents_alive > 1:
+                comm = comm / (num_agents_alive - 1)
+
+            # Mask comm_in
+            # Mask communcation from dead agents
+            comm = comm * agent_mask
+            # Mask communication to dead agents
+            comm = comm * agent_mask_transpose
+
+            # Combine all of C_j for an ith agent which essentially are h_j
+            comm_sum = comm.sum(dim=1)
+            c = self.C_modules[i](comm_sum)
+
+
+            if self.args.recurrent:
+                # skip connection - combine comm. matrix and encoded input for all agents
+                inp = x + c
+
+                inp = inp.view(batch_size * n, self.hid_size)
+
+                output = self.f_module(inp, (hidden_state, cell_state))
+
+                hidden_state = output[0]
+                cell_state = output[1]
+
+            else: # MLP|RNN
+                # Get next hidden state from f node
+                # and Add skip connection from start and sum them
+                hidden_state = sum([x, self.f_modules[i](hidden_state), c])
+                hidden_state = self.tanh(hidden_state)
+
+        # v = torch.stack([self.value_head(hidden_state[:, i, :]) for i in range(n)])
+        # v = v.view(hidden_state.size(0), n, -1)
+        value_head = self.value_head(hidden_state)
+        h = hidden_state.view(batch_size, n, self.hid_size)
+
+        if self.continuous:
+            action_mean = self.action_mean(h)
+            action_log_std = self.action_log_std.expand_as(action_mean)
+            action_std = torch.exp(action_log_std)
+            # will be used later to sample
+            action = (action_mean, action_log_std, action_std)
+        else:
+            # discrete actions
+            action = [F.log_softmax(head(h), dim=-1) for head in self.heads]
+
+        if self.args.recurrent:
+            return action, value_head, (hidden_state.clone(), cell_state.clone())
+        else:
+            return action, value_head
+
+    def init_hidden(self, batch_size):
+        """
+        Function to initialize the hidden states and cell states
+        """
+        return tuple(( torch.zeros(batch_size * self.nagents, self.hid_size, requires_grad=True),
+                       torch.zeros(batch_size * self.nagents, self.hid_size, requires_grad=True)))
+
